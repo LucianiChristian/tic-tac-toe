8a7009ce640ab80dc9d57bd582a737c118f46a30
LucianiChristian <luciani_christian@yahoo.com>
2022-06-17 19:38:36 -0500

    Step 2 Complete.

---
+++ script.js
@@ -0,0 +1,18 @@
+const Player = (name, mark) => {
+    const getName = () => name;
+    const getMark  = () => mark;
+
+    return {getName, getMark};
+}
+
+const gameBoard = (() => {
+    const board = ['X', '', 'O', '', 'X', 'X', '', '', ''];
+
+    const display = () => board;
+
+    return {display};
+})();
+
+const game = () => {
+
+};

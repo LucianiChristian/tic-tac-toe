a3aa7224e2538921eb60d3f0e0324110fc54a98d
LucianiChristian <luciani_christian@yahoo.com>
2022-06-19 11:31:48 -0500

    Step 3 In Progress : Attemping to get git in sync.

--- script.js
+++ script.js
@@ -8,24 +8,22 @@ const Player = (name, mark) => {
 const gameBoard = (() => {
     const boardState = ['X', '', 'O', '', 'X', 'X', '', '', ''];
 
-<<<<<<< HEAD
     const getBoardState = () => [...boardState];
     const setBoardState = (index, mark) => {
         boardState[index] = mark; 
     }
 
     return {getBoardState, setBoardState};
-=======
-    return {board};
->>>>>>> b68235f7ff201c4b4f020b0fbf8f98b057db2e84
 })();
 
 const gameDisplay = (() => {
     const boardDisplay = document.querySelectorAll('.square');
 
     const renderBoard = () => {
-        for(let index in gameBoard.board) {
-            boardDisplay[index].textContent = gameBoard.board[index];
+        const boardData = gameBoard.getBoardState();
+
+        for(let index in boardData) {
+            boardDisplay[index].textContent = boardData[index];
         }
     };
 

257db8964159f50c4aa4611ad6e1c0428ddedad8
LucianiChristian <luciani_christian@yahoo.com>
2022-06-21 15:48:07 -0500

    Step 4 In Progress : currentTurn, switchTurn, takeTurn added.

--- script.js
+++ script.js
@@ -6,14 +6,22 @@ const Player = (name, mark) => {
 }
 
 const boardModel = (() => {
-    const board = ['X', '', 'O', '', 'X', 'X', '', '', ''];
+    const board = ['', '', '', '', '', '', '', '', ''];
+
+    const isSquareEmpty = (index) => {
+        if(boardModel.getBoard()[index] === '') {
+            return true;
+        }
+
+        return false;
+    }
 
     const getBoard = () => [...board];
     const setSquare = (index, mark) => {
         board[index] = mark; 
     }
 
-    return {getBoard, setSquare};
+    return {getBoard, setSquare, isSquareEmpty};
 })();
 
 const boardView = (() => {
@@ -31,8 +39,39 @@ const boardView = (() => {
 })();
 
 const boardController = (() => {
-    // mark board
-        // gameBoard.setBoardState()
-        // gameDisplay.renderBoard()
+    const markBoard = (index, mark) => {
+        if(boardModel.isSquareEmpty(index) === true) {
+            boardModel.setSquare(index, mark);
+            boardView.renderBoard();
+        }
+    };
+
+    return {markBoard};
 })();
 
+const game = (() => {
+    // Create both player objects
+    const player1 = Player('player1', 'X');
+    const player2 = Player('player2', 'O');
+
+    // Set the first turn to player1 by default
+    let currentTurn = player1;
+
+    // Switches turn
+    const switchTurn = () => {
+        if(currentTurn === player1) {
+            currentTurn = player2;
+        }
+        else {
+            currentTurn = player1;
+        }
+    };
+
+    // Takes a turn
+    const takeTurn = () => {
+        boardController.markBoard(0, currentTurn.getMark());
+        switchTurn();
+    }
+
+    return {takeTurn};
+})();
